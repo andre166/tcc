50c54dccd41bf59760adc5d740b4b51a9d526347
andre166 <andremp1927@hotmail.com>
2020-09-23 14:04:12 -0300

    olho show/hide password nos inputs de redefinição de senha

--- src/pages/gerenciarAdmin/stepperAlterSenha/review.js
+++ src/pages/gerenciarAdmin/stepperAlterSenha/review.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -10,6 +10,12 @@ import Button from '@material-ui/core/Button';
 import GenerateAlert from '../../../components/errorAlert';
 import TextField from '@material-ui/core/TextField';
 import { alterUserPassword } from '../../../components/services/usuarioService';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 
 const useStyles = makeStyles((theme) => ({
   listItem: {
@@ -35,7 +41,15 @@ const useStyles = makeStyles((theme) => ({
 export default function Review( {handleNext, rowInfo}) {
 
   const classes = useStyles();
-  const [ erro, setErro ] = React.useState(false);
+  const [ erro, setErro ] = useState(false);
+
+  const [ showPassword, setShowPassword ] = useState({
+    eye1: false,
+    eye2: false
+  });
+
+
+
 
   async function onSubmit( values, action ){
 
@@ -53,8 +67,6 @@ export default function Review( {handleNext, rowInfo}) {
       setErro(false);
       handleNext();
     }
-
-
   
   }
   
@@ -82,44 +94,103 @@ export default function Review( {handleNext, rowInfo}) {
         <Form>
 
         <Grid container spacing={3} style={{padding: 10}}>
-            <TextField
-                margin="normal"
+        <FormControl style={{width: '100%'}}>
+              <InputLabel htmlFor="standard-adornment-password">Repita a nova senha</InputLabel>
+              <Input
                 fullWidth
-                id="name"
-                label="Nova senha"
-                name="senha1"
-                value={values.senha1}
+                id="standard-adornment-password"
+                type={showPassword.eye1 ? 'text' : 'password'}
                 onChange={handleChange}
-                InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        <LockIcon />
-                      </InputAdornment>
-                    ),
-                  }}
+                value={values.senha1}
+                name="senha1"
+                endAdornment={
+                  <>
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowPassword({
+                        eye1: !showPassword.eye1,
+                        eye2: showPassword.eye2,
+
+                      })}
+                    >
+                      {showPassword.eye1 ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
                   
-            />
+                </>
+                }
+                startAdornment={
+                  <InputAdornment position="start">
+                    <LockIcon />
+                  </InputAdornment>
+                }
+                
+              />
+            </FormControl>
+
             <ErrorMessage name="senha1">{(msg) => <GenerateAlert alertConfig={ {msg: msg, tipo: "warning"} } />}</ErrorMessage>
             
-            <TextField        
-                margin="normal"
+            <FormControl style={{width: '100%'}}>
+              <InputLabel htmlFor="standard-adornment-password">Repita a nova senha</InputLabel>
+              <Input
                 fullWidth
-                name="senha2"
-                label="Repita a nova senha"
-                value={values.senha2}
+                id="standard-adornment-password"
+                type={showPassword.eye2 ? 'text' : 'password'}
                 onChange={handleChange}
-                InputProps={{
-                startAdornment: (
-                    <InputAdornment position="start">
+                value={values.senha2}
+                name="senha2"
+                endAdornment={
+                  <>
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowPassword({
+                        eye1: showPassword.eye1,
+                        eye2: !showPassword.eye2,
+
+                      })}
+                      // onMouseDown={handleMouseDownPassword}
+                    >
+                      {showPassword.eye2 ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                  
+                </>
+                }
+                startAdornment={
+                  <InputAdornment position="start">
                     <LockIcon />
-                    </InputAdornment>
-                ),
-                }}
-                type="password"
-            />
+                  </InputAdornment>
+                }
+                
+              />
+            </FormControl>
+
             <ErrorMessage name="senha2">{(msg) => <GenerateAlert alertConfig={ {msg: msg, tipo: "warning"} } />}</ErrorMessage>
 
         </Grid>
+
+        {/* <FormControl className={clsx(classes.margin, classes.textField)}>
+          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+          <Input
+            id="standard-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl> */}
 
         <Grid style={{marginTop: 20}} container direction="column" justify="center" alignItems="center">
             <Button color="primary" className={classes.buttonSuccess} variant="contained" type="submit">Redefinir</Button>
